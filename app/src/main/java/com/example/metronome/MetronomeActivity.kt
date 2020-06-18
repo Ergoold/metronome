4377ffb1a70d136666161268ad1df0723b116085
Ergoold <eran1gold@gmail.com>
2020-06-18 13:26:33 +0300

    added metronome sounds

--- app/src/main/java/com/example/metronome/MetronomeActivity.kt
+++ app/src/main/java/com/example/metronome/MetronomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.metronome
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.SoundEffectConstants
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +11,34 @@ class MetronomeActivity : AppCompatActivity() {
     lateinit var bpm: EditText
     lateinit var start: Button
 
+    var delay: Long = 0
+    var on = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metronome)
 
         bpm = findViewById(R.id.bpm)
         start = findViewById(R.id.start)
+
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                start.playSoundEffect(SoundEffectConstants.CLICK)
+                handler.postDelayed(this, delay)
+            }
+        }
+
+        start.setOnClickListener {
+            if (!on) {
+                delay = 60 * 1000 / bpm.text.toString().toLong()
+                handler.postDelayed(runnable, delay)
+            } else {
+                handler.removeCallbacks(runnable)
+            }
+
+            on = !on
+            start.text = getString(if (on) R.string.stop else R.string.start)
+        }
     }
 }
