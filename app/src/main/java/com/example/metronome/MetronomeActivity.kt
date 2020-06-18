020b42782253a6b9d3e7f156af5edcacb4a36676
Ergoold <eran1gold@gmail.com>
2020-06-18 12:25:06 +0300

    initialize views in `MetronomeActivity.kt`

--- app/src/main/java/com/example/metronome/MetronomeActivity.kt
+++ app/src/main/java/com/example/metronome/MetronomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.metronome
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MetronomeActivity : AppCompatActivity() {
+    lateinit var bpm: EditText
+    lateinit var start: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metronome)
+
+        bpm = findViewById(R.id.bpm)
+        start = findViewById(R.id.start)
     }
 }
